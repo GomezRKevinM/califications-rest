78e54cad5a13dc2db72979e426170b3445c7dda7
Kevin Manuel Gómez Rojas <kgomezr2@unicartagena.edu.co>
2026-05-25 19:03:19 -0500

    remove: import innecesario

--- src/main/java/co/udc/desarrollo/web/calificationsRest/infrastructure/entrypoint/rest/dto/student/UpdateStudentRequest.java
+++ src/main/java/co/udc/desarrollo/web/calificationsRest/infrastructure/entrypoint/rest/dto/student/UpdateStudentRequest.java
@@ -1,6 +1,5 @@
 package co.udc.desarrollo.web.calificationsRest.infrastructure.entrypoint.rest.dto.student;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
