3b7400634caaf86c9af79c0b230550f67e80b885
Kevin Manuel Gómez Rojas <kgomezr2@unicartagena.edu.co>
2026-05-25 19:10:00 -0500

    feat: creacion del create student command

---
+++ src/main/java/co/udc/desarrollo/web/calificationsRest/application/service/dto/command/student/CreateStudentCommand.java
@@ -0,0 +1,15 @@
+package co.udc.desarrollo.web.calificationsRest.application.service.dto.command.student;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateStudentCommand(
+
+    @NotBlank(message = "name no puede estar vacio")
+        @Size(min = 3, message = "name debería tener al menos 3 caracteres") String name,
+
+    @NotBlank(message = "last name no puede estar vacio")
+        @Size(min = 3, message = "last name debería tener al menos 3 caracteres") String lastName )
+{
+}
