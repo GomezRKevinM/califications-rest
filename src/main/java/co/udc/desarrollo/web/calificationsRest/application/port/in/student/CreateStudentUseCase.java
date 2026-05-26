750643b13cac748f23714b38550c8b4134e023a9
Kevin Manuel Gómez Rojas <kgomezr2@unicartagena.edu.co>
2026-05-25 21:20:33 -0500

    fixed: se cambio el tipo de dato de retorno del caso de uso para crear un student - Kevin Gómez

--- src/main/java/co/udc/desarrollo/web/calificationsRest/application/port/in/student/CreateStudentUseCase.java
+++ src/main/java/co/udc/desarrollo/web/calificationsRest/application/port/in/student/CreateStudentUseCase.java
@@ -1,10 +1,10 @@
 package co.udc.desarrollo.web.calificationsRest.application.port.in.student;
 
 import co.udc.desarrollo.web.calificationsRest.application.service.dto.command.student.CreateStudentCommand;
-import co.udc.desarrollo.web.calificationsRest.domain.models.UserModel;
+import co.udc.desarrollo.web.calificationsRest.domain.models.StudentModel;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public interface CreateStudentUseCase {
-    UserModel execute(@NotNull @Valid CreateStudentCommand command);
+    StudentModel execute(@NotNull @Valid CreateStudentCommand command);
 }
